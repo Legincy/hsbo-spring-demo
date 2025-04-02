d081c0089bfc5fbc398700b31b38120c1ae5352c
Martin Peth <martin@peth.pl>
2025-04-02 11:32:43 +0200

    update(controller): added code-documentation to get finally into it :P

--- src/main/java/pl/peth/hsbo_spring_demo/controller/IndexController.java
+++ src/main/java/pl/peth/hsbo_spring_demo/controller/IndexController.java
@@ -9,11 +9,22 @@ import java.util.Map;
 
 @RestController
 public class IndexController {
+    /**
+     * Returns a greeting message that includes the provided name.
+     *
+     * @param name String : The name to include in the greeting. Defaults to "World" if not provided.
+     * @return String : A formatted greeting message: "Hello, __NAME__!"
+     */
     @GetMapping("/hello")
     public String getHello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 
+    /**
+     *  Returns the current system timestamp in milliseconds inside a HashMap
+     *
+     * @return Map<String, Long> : {"timestamp": __TIMESTAMP_IN_MILLIS__}
+     */
     @GetMapping("/timestamp")
     public Map<String, Long> getTimestamp() {
         Map<String, Long> response = new HashMap<>();
